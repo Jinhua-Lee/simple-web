ed44565642c0067bccd66d5707ccfe12bcb156f7
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-08-06 00:06:06 +0800

    日志框架调试

--- src/main/java/com/controller/filter/MyFilter.java
+++ src/main/java/com/controller/filter/MyFilter.java
@@ -1,5 +1,8 @@
 package com.controller.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +17,15 @@ import java.io.IOException;
 @WebFilter(filterName = "MyFilter", urlPatterns = "/*")
 public class MyFilter implements Filter {
 
+    private static final Logger log = LoggerFactory.getLogger(MyFilter.class);
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        log.info("我的简易框架！");
 
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
