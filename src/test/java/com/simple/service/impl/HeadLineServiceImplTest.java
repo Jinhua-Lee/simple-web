45c2e13182ddf41e366efea1015727d9c8d90f1c
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-11-16 23:59:14 +0800

    something.

--- src/test/java/com/simple/service/impl/HeadLineServiceImplTest.java
+++ src/test/java/com/simple/service/impl/HeadLineServiceImplTest.java
@@ -1,21 +1,19 @@
 package com.simple.service.impl;
 
-import com.simple.entity.HeadLine;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.pagehelper.PageInfo;
+import com.simple.entity.HeadLine;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.simpleframework.core.annotation.Service;
 
 import java.util.List;
 
 /**
  * 头条列表测试
  */
-@Service
 public class HeadLineServiceImplTest extends HeadLineServiceImpl {
 
     @Test
